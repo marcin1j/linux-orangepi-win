1fe5006fa91a45ec546919075d7ccd51eda8bb6d
Vasily Khoruzhick <anarsoul@gmail.com>
2018-10-28 15:32:38 -0700

    drm/sun4i: tcon: enable dithering on tcon0 is there's bridge connected
    RGB on A64 is 6bpc, so if bridge is connected we need to enable dithering
    
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- drivers/gpu/drm/sun4i/sun4i_tcon.c
+++ drivers/gpu/drm/sun4i/sun4i_tcon.c
@@ -284,10 +284,6 @@ static void sun4i_tcon0_mode_set_dithering(struct sun4i_tcon *tcon,
 	u32 bus_format = 0;
 	u32 val = 0;
 
-	/* XXX Would this ever happen? */
-	if (!connector)
-		return;
-
 	/*
 	 * FIXME: Undocumented bits
 	 *
@@ -305,11 +301,15 @@ static void sun4i_tcon0_mode_set_dithering(struct sun4i_tcon *tcon,
 	regmap_write(tcon->regs, SUN4I_TCON0_FRM_TBL2_REG, 0x57575555);
 	regmap_write(tcon->regs, SUN4I_TCON0_FRM_TBL3_REG, 0x7f7f7777);
 
-	/* Do dithering if panel only supports 6 bits per color */
-	if (connector->display_info.bpc == 6)
+	/* Do dithering if there's bridge connected */
+	if (!connector)
 		val |= SUN4I_TCON0_FRM_CTL_EN;
 
-	if (connector->display_info.num_bus_formats == 1)
+	/* Do dithering if panel only supports 6 bits per color */
+	if (connector && connector->display_info.bpc == 6)
+		val |= SUN4I_TCON0_FRM_CTL_EN;
+
+	if (connector && connector->display_info.num_bus_formats == 1)
 		bus_format = connector->display_info.bus_formats[0];
 
 	/* Check the connection format */
